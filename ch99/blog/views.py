0bc4f567d1ef19b64bb980b0007f70fe14dfa165
hesoo98 <hesoo98@users.noreply.github.com>
2021-01-13 10:57:32 +0900

    검색 폼

--- ch99/blog/views.py
+++ ch99/blog/views.py
@@ -1,15 +1,20 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
+from django.views.generic import FormView
+from .forms import PostSearchForm
+from django.db.models import Q
+from django.shortcuts import render
 from django.conf import settings
-
 from .models import Post
+
 
 class PostLV(ListView):
     model = Post
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
     paginate_by = 2
+
 
 class PostDV(DetailView):
     model = Post
@@ -22,29 +27,36 @@ class PostDV(DetailView):
         context['disqus_title'] = f"{self.object.slug}"
         return context
 
+
 class PostAV(ArchiveIndexView):
     model = Post
     date_field = 'modify_dt'
+
 
 class PostYAV(YearArchiveView):
     model = Post
     date_field = 'modify_dt'
     make_object_list = True
 
+
 class PostMAV(MonthArchiveView):
     model = Post
     date_field = 'modify_dt'
+
 
 class PostDAV(DayArchiveView):
     model = Post
     date_field = 'modify_dt'
 
+
 class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
 
+
 class TagCloudTV(TemplateView):
     template_name = 'taggit/taggit_cloud.html'
+
 
 class TaggedObjectLV(ListView):
     template_name = 'taggit/taggit_post_list.html'
@@ -57,3 +69,19 @@ class TaggedObjectLV(ListView):
         context = super().get_context_data(**kwargs)
         context['tagname'] = self.kwargs['tag']
         return context
+
+
+class SearchFormView(FormView):
+    form_class = PostSearchForm
+    template_name = 'blog/post_search.html'
+
+    def form_valid(self, form):
+        searchWord = form.cleaned_data['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=searchWord) | Q(description__icontains=searchWord) |
+                                        Q(content__icontains=searchWord)).distinct()
+        context = {}
+        context['form'] = form
+        context['search_term'] = searchWord
+        context['object_list'] = post_list
+
+        return render(self.request, self.template_name, context)  # No Redirection
